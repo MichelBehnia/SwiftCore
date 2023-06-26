9520a8e302dd9aac12d995f5721eecb744244b10
MichelBehnia <michelbehnia2003@gmail.com>
2023-06-26 01:58:41 -0400

    Revert "got rid of importing unused part of react-scroll package"
    This reverts commit 2ef444c5f0a2208b2e25dd4b9b0afa7f96fe3a66.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Main from "./components/Main";
 import Info from "./components/Info";
 import Order from "./components/Order";
 import Contact from "./components/Contact";
-import Link from 'react-scroll'
+
+import { Link, animateScroll as Scroll} from 'react-scroll'
 
 function App() {
   return (
