0c6d2b9e2ae178986580f8071990e60e0f67ed03
MichelBehnia <michelbehnia2003@gmail.com>
2023-06-26 01:34:41 -0400

    updating emailjs info and updated how it works video

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
     const sendEmail = (e) => {
       e.preventDefault()
   
-      emailjs.sendForm('service_8t653pp', 'template_2fjsy2n', form.current, 'ACQ3Hjhht80KREU5i')
+      emailjs.sendForm(process.env.REACT_APP_SERVICE_KEY, 'template_u0d7gvp', form.current, process.env.REACT_APP_API_KEY)
         .then((result) => {
             console.log(result.text)
             alert("Thank you for contacting us, expect a response as soon as possible")
