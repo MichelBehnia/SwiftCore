134799cb13bce38a0fdf045bf03e0455713e0da0
MichelBehnia <michelbehnia2003@gmail.com>
2023-06-29 17:13:48 -0400

    updated navbar and fixed padding on contact component

--- src/App.js
+++ src/App.js
@@ -11,19 +11,19 @@ function App() {
       {/* maybe put all the code below in a navbar component along with the import up top */}
       <div className="z-10 bg-slate-950 sticky top-0 border-b rounded-lg flex justify-between items-center h-24 max-w-[1440px] mx-auto px-4">
         <h1 className="w-full text-3xl font-bold text-violet-400">SwiftCore</h1>
-        <ul className="text-white flex">
-          <li className="p-4">
+        <ul className="text-white font-bold flex">
+          <li className="p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
             <Link activeClass="active" smooth spy to="Info">
               Info
             </Link>
               
           </li>
-          <li className="p-4">
+          <li className="p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
             <Link activeClass="active" smooth spy to="Order">
               Purchase
             </Link>
           </li>
-          <li className="p-4">
+          <li className="p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
             <Link activeClass="active" smooth spy to="Contact">
               Contact
             </Link>
@@ -43,7 +43,7 @@ function App() {
         <Order />
       </div>
       <div className="z-0">
-        <section id="Contact"></section>
+        <section className="pt-5" id="Contact"></section>
         <Contact />
       </div>
     </div>
